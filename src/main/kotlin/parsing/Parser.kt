7c0ad4c400085e6b475790f8864403234db9e809
Sam Sifiso Mndebele <sams.mndebele@gmail.com>
2025-09-11 22:48:28 +0200

    Refactor: Add `main` function for parser and print tokens
    This commit introduces a `main` function within `Parser.kt` to instantiate and run the `Lexer` and `Parser`.
    
    Key changes:
    - **Added `main` function:**
        - Reads assembly source code from `src/main/kotlin/main.asm`.
        - Includes basic error handling for `FileNotFoundException`, printing an error message and exiting if the file is not found.
        - Initializes `Lexer`, `Memory`, and `Parser` instances.
    - **Token Printing in `Parser`:**
        - The `Parser`'s `init` block now iterates through the tokens provided by the `Lexer` and prints each token to the console.
    - **Imports:**
        - Added `java.io.File`, `java.io.FileNotFoundException`, and `kotlin.system.exitProcess` imports for the `main` function's file handling and process control.

--- src/main/kotlin/parsing/Parser.kt
+++ src/main/kotlin/parsing/Parser.kt
@@ -3,9 +3,36 @@ package parsing
 import cpu.Memory
 import lexical.Lexer
 import model.Section
+import java.io.File
+import java.io.FileNotFoundException
+import kotlin.system.exitProcess
 
 class Parser(private val lexer: Lexer, private val memory: Memory) {
     private var currentSection = Section.CODE
     private val symbolTable = mutableMapOf<String, UInt>() // symbol -> offset within data segment
     private var dataOffset = 0L // Current offset within the data segment
+
+    init {
+        println("Tokens:")
+        lexer.getTokens().forEach {
+            println(it)
+        }
+    }
+}
+
+
+fun main() {
+    val src = try {
+        File("src/main/kotlin/main.asm").readText()
+    } catch (e: FileNotFoundException) {
+        println("Error: ${e.message}, Source file not found.")
+        println("Please provide a valid path as a command-line argument or make sure the default file exists.")
+        exitProcess(1)
+    }
+
+    val lexer = Lexer(src)
+    val memory = Memory()
+    val parser = Parser(lexer, memory)
+
+
 }
